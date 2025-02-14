98a4efa3b897a99e5e865347422f81610bd0ceff
Takuto Odaira <takuto@wlan-napt-006.u-aizu.ac.jp>
2025-02-14 20:31:21 +0900

    added get_elements method to FileLoader class

--- src/FileLoader.py
+++ src/FileLoader.py
@@ -21,9 +21,10 @@ class FileLoader:
                 raise FileNotFoundError(f"Error: The file at {self.file_path} does not exist.")
                 
             # load file
-            loaded_data = pd.read_csv(self.file_path)
-            print(f"File: {self.file_path} loaded successfully.")
-            return loaded_data
+            self.loaded_data = pd.read_csv(self.file_path)
+            print(f"File loaded successfully: {self.file_path} ")
+            self.size = loaded_data.shape # store size of data
+            return self.loaded_data
             
         # error catch
         except FileNotFoundError as e: # file not found error
@@ -33,3 +34,15 @@ class FileLoader:
         except Exception as e:
             print(f"An unexpected error occurred: {e}")  # other error
         return None
+            
+    
+    def get_elements(self):
+        """
+        This method checks if data is loaded then returns elements from header
+        """
+        if hasattr(self, 'loaded_data'): # check if data is loaded in 'load_csv' method
+            elements = self.loaded_data.columns.tolist() # get elements in header and stores in list
+            return elements
+        else:
+            raise ValueError("CSV file not loaded. Please load the CSV file first.")
+        
