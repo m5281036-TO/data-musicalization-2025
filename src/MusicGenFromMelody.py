d0f127bb0f8cc9d0e313c222ce99db975c71858b
Takuto Odaira <takuto@MacBook-Pro-BIRD.local>
2025-05-08 12:50:41 +0900

    fix: MusicGebFromMelody.py: squeeze wav dimesntion to avoid error

--- src/MusicGenFromMelody.py
+++ src/MusicGenFromMelody.py
@@ -12,24 +12,35 @@ class MusicGenFromMelody:
     
     """
     
-    def __init__ (self, output_dir, duration=8):
+    def __init__ (self, input_dir, output_dir, duration=8):
         os.environ["PATH"] += os.pathsep + '/opt/homebrew/bin/ffmpeg'
-        print(shutil.which("ffmpeg"))  # if None, ffmpeg do not found
+        if shutil.which("ffmpeg")  == None: # if None, ffmpeg do not found
+            raise ValueError("ffmpeg does not installed or path is not enabled")
+        
+        self.input_dir = input_dir
         self.output_dir = output_dir
-        self.duration = duration
+        
+        self.model = MusicGen.get_pretrained('melody')
+        self.model.set_generation_params(duration=duration)
+    
         
     
     def music_gen_from_melody (self, melody_path, description, idx):
         print(f"-------- Generating --------\nprompt: {description}\nmelody: {melody_path}\n")
-
-        model = MusicGen.get_pretrained('melody')
-        model.set_generation_params(duration=self.duration)
-
+        
         melody, sr = torchaudio.load(melody_path)
         # generates using the melody from the given audio and the provided descriptions.
-        wav = model.generate_with_chroma([description], melody, sr)
-
+        print("\nmodel created, generating audio...")
+        wav = self.model.generate_with_chroma([description], melody, sr)
+        
+        # set wav dimension
+        if wav.dim() == 3 and wav.size(0) == 1:
+            wav = wav.squeeze(0)  # [1, C, T] → [C, T]
+        elif wav.dim() > 2:
+            raise ValueError(f"Unexpected wav shape: {wav.shape}")
+        print("wav dimension ok")
+        
         # loudness normalization at -14 db LUFS.
-        audio_path = os.path.join(self.output_dir, f"{idx}.wav")
-        audio_write(f'{audio_path}', wav.cpu(), model.sample_rate, strategy="loudness")
+        output_audio_path = os.path.join(self.output_dir, f"{idx}.wav")
+        audio_write(f'{output_audio_path}', wav.cpu(), self.model.sample_rate, strategy="loudness")
 
